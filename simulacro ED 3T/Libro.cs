7979180134b7dfda7a881379139e103c245b4b51
Wilman Martins <wilmanmartinscl@digitechfp.com>
2026-05-05 12:22:57 +0200

    Creacion de la clase Libro y el txt respuestas

---
+++ simulacro ED 3T/Libro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simulacro_ED_3T
+{
+    internal class Libro
+    {
+        private string titulo;
+        private string autor;
+        private int anyo;
+        private bool disponible;
+
+        public Libro(string titulo, string autor, int anyo, bool disponible)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anyo = anyo;
+            this.disponible = disponible;
+        }
+
+        public string Titulo()
+        {
+            return titulo;
+        }
+
+        public string Autor()
+        {
+            return autor;
+        }
+
+        public int Anyo()
+        {
+            return anyo;
+        }
+
+        public bool isDisponible()
+        {
+            return disponible;
+        }
+
+        public string toString()
+        {
+            return "Titulo: " + titulo + ", Autor" + autor + ", Año: " + anyo;
+        }
+    }
+}
